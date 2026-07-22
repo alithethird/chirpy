85fa2a7b994ba7ce58b1024b981b2c5deac40286
Ali Ugur <aliugur997@gmail.com>
2026-07-22 08:12:53 +0300

    feat: healthcheck

--- main.go
+++ main.go
@@ -11,7 +11,12 @@ func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
 func main() {
 	mux := http.NewServeMux()
 	srv := http.Server{Addr: ":8080", Handler: mux}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	for {
 		srv.ListenAndServe()
 	}
